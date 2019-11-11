518ca9f32fdfe894d04258a5fe6a8038a0bc2cdc
Sebastian <sebatiantords@gmail.com>
2019-11-11 13:20:04 +0100

    max_contour added

--- Gesturnary.py
+++ Gesturnary.py
@@ -8,15 +8,21 @@ while True:
     ret, frame = cap.read()
 
     # Blur image
-    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    blurred_frame = cv2.bilateralFilter(frame,9,75,75)
+
+
 
     # Convert from BGR to HSV
     hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
 
     # Defining the range of color in HSV
     # Green
-    lower_hand = np.array([30, 150, 80])
-    upper_hand = np.array([125, 255, 255])
+    lower_hand = np.array([40, 100, 80])
+    upper_hand = np.array([80, 255, 255])
+
+     #lower_hand = np.array([30, 150, 80])
+     #upper_hand = np.array([125, 255, 255])
+
     # Blue
     # lower_hand = np.array([38, 85, 0])
     # upper_hand = np.array([120, 255, 255])
@@ -29,21 +35,25 @@ while True:
     cv2.drawContours(frame, contours, -2, (0, 255, 0), 3)
 
     if contours:
-        cnt = contours[0]
-        epsilon = 0.0005 * cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, True)
+        # assume largest contour is the one of interest
+        max_contour = max(contours, key=cv2.contourArea)
 
-        # hull = cv2.convexHull(cnt)
+        epsilon = 0.01 * cv2.arcLength(max_contour, True)
+        max_contour = cv2.approxPolyDP(max_contour, epsilon, True)
 
-        hull = cv2.convexHull(approx, returnPoints=False)
-        defects = cv2.convexityDefects(approx, hull)
+        # determine convex hull & convexity defects of the hull
+        hull = cv2.convexHull(max_contour, returnPoints=False)
+        defects = cv2.convexityDefects(max_contour, hull)
+
+
+        # Camera
 
         if defects is not None:
             for i in range(defects.shape[0]):
                 s, e, f, d = defects[i, 0]
-                start = tuple(approx[s][0])
-                end = tuple(approx[e][0])
-                far = tuple(approx[f][0])
+                start = tuple(max_contour[s][0])
+                end = tuple(max_contour[e][0])
+                far = tuple(max_contour[f][0])
                 cv2.line(frame, start, end, [255, 255, 0], 2)
                 cv2.circle(frame, far, 5, [0, 0, 255], -1)
 
